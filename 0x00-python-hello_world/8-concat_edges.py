90bbc8e59ce2beff781bad98b795b5c28b5ce1e9
Minah4code <aminahmuhammed5@gmail.com>
2024-01-15 07:24:01 -0800

    0x00. Python - Hello, World

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is interpreted, interactive and object-oriented\
+ paradigm, harmonizing impressive capabilities with a highly legible syntax."
+str = str[39:67] + str[107:112] + str[:6]
+print(str)
