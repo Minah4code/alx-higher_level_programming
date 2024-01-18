fd34e102db4a82b507bc1fa95cd458058308223d
Minah4code <aminahmuhammed5@gmail.com>
2024-01-18 08:37:26 -0800

    0x02-python-import_modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 # 2-args.py
 # Brennan D Baraban <375@holbertonschool.com>
 
@@ -15,4 +15,3 @@ if __name__ == "__main__":
         print("{} arguments:".format(count))
     for i in range(count):
         print("{}: {}".format(i + 1, sys.argv[i + 1]))
-       
